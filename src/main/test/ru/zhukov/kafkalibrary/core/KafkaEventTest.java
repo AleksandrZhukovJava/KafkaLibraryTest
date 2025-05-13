88716f429355152bda5e6b7df3100f8fbba9a3da
Zhukov Aleksandr <a.zhukov@uzum.com>
2025-05-13 20:11:09 +0200

    cicd fix 7

---
+++ src/main/test/ru/zhukov/kafkalibrary/core/KafkaEventTest.java
@@ -0,0 +1,13 @@
+package ru.zhukov.kafkalibrary.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KafkaEventTest {
+
+    @Test
+    void getType() {
+        fail();
+    }
+}
